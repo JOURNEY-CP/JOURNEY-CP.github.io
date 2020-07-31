627682fdd6c723f567fae8a6717474f27a705bd5
VADDAVALLI SAI SITA RAM <saisitaramvaddavalli@gmail.com>
2020-07-31 11:25:00 +0530

    added css

--- src/components/Sumlinks.js
+++ src/components/Sumlinks.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import sums from '../data/august-2020.json';
-
+import '../CSS/layout.css'
 export default class Sumlinks extends Component{
     constructor(props) {
         super(props);
         this.state={
             currdate:new Date(),
             months:["January","February","March","April","May","June","July","August","September","October","November","December"]
-            
-            
           }
-
     }
+
    singleDaySums=props=>(
     sums[props.date].map((sum,i)=>{
       return <a href={sum}>problem{i+1}</a>
    },0)
    )
 
-
    sumSection = () =>(
    
-    [...Array(this.state.currdate.getDate()).keys()].map(date=>{
+    [...Array(this.state.currdate.getDate()).keys()].reverse().map(date=>{
 
     return <div>
       <div className="three columns header-col">
@@ -30,21 +27,16 @@ export default class Sumlinks extends Component{
       <div className="nine columns main-col">
             <div className="row item">
                 <div className="twelve columns">
-                 <this.singleDaySums date={this.state.currdate.getDate()} />
-                 {/* sums[date].map((sum,i)=>{
-                   return  <a href={sum}>problem{i+1}</a>
-                  },0) */}
+                 <this.singleDaySums date={date+1} />
                 </div>
             </div>
       </div>
-
     </div>
      
     
    })
    )
-      
-    
+        
     render(){
         return(
             <div className="links">
