8065a4732f9d13face75af261ea729d98e88605f
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-07-31 09:36:03 +0530

    Added today sums

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
 import Sumlinks from './components/Sumlinks';
+import TodaySums from './components/TodaySums';
 function App() {
   return (
     <div className="App">
+      <TodaySums/>
       <Sumlinks />
     </div>
   );
--- src/components/Sumlinks.js
+++ src/components/Sumlinks.js
@@ -1,50 +1,41 @@
 import React, { Component } from 'react';
 import sums from '../data/august-2020.json';
+import './SumLinks.css';
 
 export default class Sumlinks extends Component{
     constructor(props) {
         super(props);
         this.state={
             currdate:new Date(),
-            months:["January","February","March","April","May","June","July","August","September","October","November","December"]
-            
-            
+            months:["January","February","March","April","May","June","July","August","September","October","November","December"]         
           }
-
     }
-   singleDaySums=props=>(
-    sums[props.date].map((sum,i)=>{
-      return <a href={sum}>problem{i+1}</a>
-   },0)
-   )
 
+    singleDaySums=props=>(
+        sums[props.date].map((sum,i)=>{
+            return <a href={sum}>problem{i+1}</a>
+        },0)
+    )
 
    sumSection = () =>(
-   
     [...Array(this.state.currdate.getDate()).keys()].map(date=>{
-
-    return <div>
-      <div className="three columns header-col">
-          <h1><span>{this.state.months[this.state.currdate.getMonth()]} {date+1},{this.state.currdate.getFullYear()}</span></h1>
-      </div>
-      <div className="nine columns main-col">
-            <div className="row item">
-                <div className="twelve columns">
-                 <this.singleDaySums date={this.state.currdate.getDate()} />
-                 {/* sums[date].map((sum,i)=>{
-                   return  <a href={sum}>problem{i+1}</a>
-                  },0) */}
+        return (
+            <div>
+                <div className="three columns header-col">
+                    <h1><span>{this.state.months[this.state.currdate.getMonth()]} {date+1},{this.state.currdate.getFullYear()}</span></h1>
+                </div>
+                <div className="nine columns main-col">
+                    <div className="row item">
+                        <div className="twelve columns">
+                        <this.singleDaySums date={this.state.currdate.getDate()} />
+                        </div>
+                    </div>
                 </div>
             </div>
-      </div>
-
-    </div>
-     
-    
-   })
+        )
+    })
    )
-      
-    
+
     render(){
         return(
             <div className="links">
@@ -52,7 +43,4 @@ export default class Sumlinks extends Component{
             </div>
         )
     }
-
-
-
 }
